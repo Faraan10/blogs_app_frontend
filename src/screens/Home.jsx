43e164919ac6994241c07b57b30d091ed62510e0
Muhammed Faraan <faraanshaik19@gmail.com>
2024-12-07 13:08:59 +0530

    styles commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -18,7 +18,43 @@ const Home = () => {
 
   return (
     <>
-      <h1>News Blogs Application</h1>
+      <h1 className="header">News Blogs</h1>
+
+      <form className="max-w-md mx-auto form-align">
+        <label
+          for="default-search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="  Search Blogs"
+            required
+          />
+        </div>
+      </form>
+
       <div className="container-fluid">
         <div className="row">
           {data?.articles?.map((article) => (
@@ -26,14 +62,19 @@ const Home = () => {
               className="col-sm-6 col-md-4 col-lg-3 custom-card"
               key={article.publishedAt}
             >
-              <img src={article.urlToImage} width="200px" />
+              <img src={article.urlToImage} className="article-image" />
               <div className="card-body">
-                <h3>{article.title}</h3>
-                <p>{article.description}</p>
-                <p>Written by {article.author}</p>
-                <p>
+                <h3 className="article-title">{article.title}</h3>
+                <p className="article-description">{article.description}</p>
+                <p className="article-author">
+                  Written by{" "}
+                  <span className="article-author-span">{article.author}</span>
+                </p>
+                <p className="article-date">
                   Published on{" "}
-                  {new Date(article.publishedAt).toLocaleDateString()}
+                  <span className="article-date-span">
+                    {new Date(article.publishedAt).toLocaleDateString()}
+                  </span>
                 </p>
               </div>
             </div>
