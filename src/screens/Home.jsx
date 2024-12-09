fc6bc751cff719226ca39d62af2ad9e9fc527ceb
Muhammed Faraan <faraanshaik19@gmail.com>
2024-12-09 18:30:16 +0530

    spinner commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import getData from "../api/getData";
 import { useEffect } from "react";
+import Spinner from "../components/Spinner";
 
 const Home = () => {
   const [data, setData] = useState();
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const newsData = async () => {
     const response = await getData();
+    setLoading(false);
     setData(response);
   };
 
@@ -23,7 +26,7 @@ const Home = () => {
 
       <form className="max-w-md mx-auto form-align">
         <label
-          for="default-search"
+          htmlFor="default-search"
           className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
           Search
@@ -39,9 +42,9 @@ const Home = () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
@@ -60,36 +63,42 @@ const Home = () => {
 
       <div className="container-fluid">
         <div className="row">
-          {data?.articles
-            ?.filter((article) => {
-              return search.toLowerCase() === ""
-                ? article
-                : article.title.toLowerCase().includes(search);
-            })
-            .map((article) => (
-              <div
-                className="col-sm-6 col-md-4 col-lg-3 custom-card"
-                key={article.publishedAt}
-              >
-                <img src={article.urlToImage} className="article-image" />
-                <div className="card-body">
-                  <h3 className="article-title">{article.title}</h3>
-                  <p className="article-description">{article.description}</p>
-                  <p className="article-author">
-                    Written by{" "}
-                    <span className="article-author-span">
-                      {article.author}
-                    </span>
-                  </p>
-                  <p className="article-date">
-                    Published on{" "}
-                    <span className="article-date-span">
-                      {new Date(article.publishedAt).toLocaleDateString()}
-                    </span>
-                  </p>
-                </div>
-              </div>
-            ))}
+          {loading ? (
+            <Spinner />
+          ) : (
+            data?.articles
+              ?.filter((article) => {
+                return search.toLowerCase() === ""
+                  ? article
+                  : article.title.toLowerCase().includes(search);
+              })
+              .map((article) => (
+                <a
+                  href={article.url}
+                  className="col-sm-6 col-md-4 col-lg-3 custom-card"
+                  target="_blank"
+                  key={article.title}
+                >
+                  <img src={article.urlToImage} className="article-image" />
+                  <div className="card-body">
+                    <h3 className="article-title">{article.title}</h3>
+                    <p className="article-description">{article.description}</p>
+                    <p className="article-author">
+                      Written by{" "}
+                      <span className="article-author-span">
+                        {article.author}
+                      </span>
+                    </p>
+                    <p className="article-date">
+                      Published on{" "}
+                      <span className="article-date-span">
+                        {new Date(article.publishedAt).toLocaleDateString()}
+                      </span>
+                    </p>
+                  </div>
+                </a>
+              ))
+          )}
         </div>
       </div>
     </>
